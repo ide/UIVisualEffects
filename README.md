e782a3903655a1831bc7004c9ddce309d8ad6b16
James Ide <ide@jameside.com>
2014-06-14 23:08:32 -0700

    Example with blur and vibrancy effects applied to text and images

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
 UIVisualEffects
 ===============
 
-An example of how to use UIVisualEffectView with UIBlurEffect and UIVibrancyEffect in iOS 8.
+Showing how to use UIVisualEffectViews with UIBlurEffect and UIVibrancyEffect in iOS 8. The example is written in Swift.
+
+<img src="Screenshots/UIVisualEffects.png" width="320" height="568" alt="Screenshot">
+
+UIVisualEffectView and UIVisualEffect
+-------------------------------------
+
+iOS 8 lets you blur your UI with UIVisualEffectView and a UIVisualEffect of your choice. You can choose amongst various blurring and vibrancy effects as shown in the screenshot above.  The API is particular about how you compose the views and which colors and image rendering modes you pick; otherwise it may not display the effect you expect. This example shows you how to use UIVisualEffectView with UIBlurEffect and UIVibrancyEffect.
+
+### Blurring
+
+There are three types of blurring: extra light, light, and dark. You specify one of those three types when creating a UIBlurEffect, which you then use to create a UIVisualEffectView. **UIVisualEffectView blurs the views underneath it. It does not blur subviews of its contentView.** Note that you should add subviews to the UIVisualEffectView's contentView rather than itself.
+
+### Vibrancy
+
+The vibrancy effect lets the content underneath a blurred view show through more vibrantly. It looks pretty but can make text and icons harder to read so use it judiciously. You specify a UIBlurEffect when creating a UIVibrancyEffect. The UIBlurEffect should be the one you used to create the blurred UIVisualEffectView. You then use the UIVibrancyEffect to create another UIVisualEffectView that you add to the blurred UIVisualEffectView's contentView.
+
+To render vibrant text, create a UILabel with a specially picked text color that depends on the blur effect underneath. **For the default appearance, use 0.25 white with extra light blur, 0.6 white with light blur, and 0.64 white with dark blur.** With these background colors your text will appear the same as an icon would. You can use different colors if you want your text to stand out more or take on a hue. However, some colors are rendered either opaque or invisible. Finally, add the UILabel to the vibrancy UIVisualEffectView's contentView.
+
+For vibrant images, create a UIImage that is treated as a template mask with `UIImageRenderingMode.AlwaysTemplate`. One side effect is that the colors in your image are ignored. However, this lets you easily use your image in a vibrancy UIVisualEffectView. As with text, you add the UIImage to the vibrancy UIVisualEffectView's contentView.
+
+### Canceling Vibrancy
+
+You can cancel the vibrancy effect with a UILabel whose text color is rendered invisible with the blur effect underneath.
+
+<img src="Screenshots/CancelUIVibrancyEffect.png" width="320" height="568" alt="Screenshot">
+
+For an "invisible" text color, you can use white for extra light or light blur and black for dark blur. Add the UILabel to a vibrancy UIVisualEffectView's contentView and position it on top of vibrant text or a vibrant image.
+
+This effect is not demonstrated in the example, but the screenshot above should give you a sense of what it looks like.
---
+++ UIVisualEffects/ViewController.swift
@@ -0,0 +1,163 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let scrollView = UIScrollView(frame: self.view.bounds)
+        scrollView.delegate = self
+        scrollView.bounces = false
+        scrollView.backgroundColor = UIColor.blackColor()
+        self.view.addSubview(scrollView)
+
+        let yosemiteImage = UIImage(named: "Yosemite")
+        let backgroundImageView = UIImageView(image: yosemiteImage)
+        scrollView.addSubview(backgroundImageView)
+
+        scrollView.contentSize = yosemiteImage.size
+        let minHorizScale = scrollView.bounds.width / yosemiteImage.size.width
+        let minVertScale = scrollView.bounds.height / yosemiteImage.size.height
+        scrollView.minimumZoomScale = min(minHorizScale, minVertScale)
+        scrollView.maximumZoomScale = 1.0
+        scrollView.zoomScale = max(minHorizScale, minVertScale)
+        scrollView.contentOffset = CGPoint(
+            x: (scrollView.contentSize.width - scrollView.bounds.width) / 2,
+            y: (scrollView.contentSize.height - scrollView.bounds.height) / 2)
+        setContentInsetToCenterScrollView(scrollView)
+
+        let darkBlur = UIBlurEffect(style: .Dark)
+        let darkBlurView = UIVisualEffectView(effect: darkBlur)
+        darkBlurView.userInteractionEnabled = false
+        self.view.addSubview(darkBlurView)
+
+        let lightBlur = UIBlurEffect(style: .Light)
+        let lightBlurView = UIVisualEffectView(effect: lightBlur)
+        lightBlurView.userInteractionEnabled = false
+        self.view.addSubview(lightBlurView)
+
+        let extraLightBlur = UIBlurEffect(style: .ExtraLight)
+        let extraLightBlurView = UIVisualEffectView(effect: extraLightBlur)
+        extraLightBlurView.userInteractionEnabled = false
+        self.view.addSubview(extraLightBlurView)
+
+        let blurAreaAmount = self.view.bounds.height / 4
+        var remainder: CGRect
+        (darkBlurView.frame, remainder) = self.view.bounds.rectsByDividing(blurAreaAmount, fromEdge: CGRectEdge.MaxYEdge)
+        (lightBlurView.frame, remainder) = remainder.rectsByDividing(blurAreaAmount, fromEdge: CGRectEdge.MaxYEdge)
+        (extraLightBlurView.frame, remainder) = remainder.rectsByDividing(blurAreaAmount, fromEdge:CGRectEdge.MaxYEdge)
+
+
+        let extraLightVibrancyView = vibrancyEffectView(forBlurEffectView: extraLightBlurView)
+        extraLightBlurView.contentView.addSubview(extraLightVibrancyView)
+
+        let lightVibrancyView = vibrancyEffectView(forBlurEffectView: lightBlurView)
+        lightBlurView.contentView.addSubview(lightVibrancyView)
+
+        let darkVibrancyView = vibrancyEffectView(forBlurEffectView: darkBlurView)
+        darkBlurView.contentView.addSubview(darkVibrancyView)
+
+
+        let cameraButton = tintedIconButton(iconNamed: "Camera")
+        cameraButton.center = extraLightVibrancyView.convertPoint(extraLightVibrancyView.center, fromView: extraLightVibrancyView.superview)
+        extraLightVibrancyView.contentView.addSubview(cameraButton)
+
+        let geniusButton = tintedIconButton(iconNamed: "Genius")
+        geniusButton.center = lightVibrancyView.convertPoint(lightVibrancyView.center, fromView: lightVibrancyView.superview)
+        lightVibrancyView.contentView.addSubview(geniusButton)
+
+        let bitcoinButton = tintedIconButton(iconNamed: "Bitcoin")
+        bitcoinButton.center = darkVibrancyView.convertPoint(darkVibrancyView.center, fromView: darkVibrancyView.superview)
+        darkVibrancyView.contentView.addSubview(bitcoinButton)
+
+
+        let extraLightTitleLabel = titleLabel(text: "Extra Light Blur")
+        extraLightTitleLabel.textColor = UIColor(white: 0.25, alpha: 1)
+        extraLightVibrancyView.contentView.addSubview(extraLightTitleLabel)
+
+        let lightTitleLabel = titleLabel(text: "Light Blur")
+        lightTitleLabel.textColor = UIColor(white: 0.6, alpha: 1)
+        lightVibrancyView.contentView.addSubview(lightTitleLabel)
+
+        let darkTitleLabel = titleLabel(text: "Dark Blur")
+        darkTitleLabel.textColor = UIColor(white: 0.64, alpha: 1)
+        darkVibrancyView.contentView.addSubview(darkTitleLabel)
+
+
+        addVibrantStatusBarBackground(extraLightBlur)
+    }
+
+    func vibrancyEffectView(forBlurEffectView blurEffectView:UIVisualEffectView) -> UIVisualEffectView {
+        let vibrancy = UIVibrancyEffect(forBlurEffect: blurEffectView.effect as UIBlurEffect)
+        let vibrancyView = UIVisualEffectView(effect: vibrancy)
+        vibrancyView.userInteractionEnabled = false
+        vibrancyView.frame = blurEffectView.bounds
+        vibrancyView.autoresizingMask = .FlexibleWidth | .FlexibleHeight
+        return vibrancyView
+    }
+
+    func tintedIconButton(iconNamed iconName: String) -> UIButton {
+        let iconImage = UIImage(named: iconName).imageWithRenderingMode(.AlwaysTemplate)
+        let borderImage = UIImage(named: "ButtonRoundRect").imageWithRenderingMode(.AlwaysTemplate)
+
+        let button = UIButton(frame: CGRect(origin: CGPointZero, size: borderImage.size))
+        button.setBackgroundImage(borderImage, forState: .Normal)
+        button.setImage(iconImage, forState: .Normal)
+        return button
+    }
+
+    func titleLabel(#text: String) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.sizeToFit()
+        label.frame.origin = CGPoint(x: 12, y: 12)
+        return label
+    }
+
+    func addVibrantStatusBarBackground(effect: UIBlurEffect) {
+        let statusBarBlurView = UIVisualEffectView(effect: effect)
+        statusBarBlurView.frame = UIApplication.sharedApplication().statusBarFrame
+        statusBarBlurView.autoresizingMask = .FlexibleWidth
+
+        let statusBarVibrancyView = vibrancyEffectView(forBlurEffectView: statusBarBlurView)
+        statusBarBlurView.contentView.addSubview(statusBarVibrancyView)
+
+        let statusBar = UIApplication.sharedApplication().valueForKey("statusBar") as UIView
+        statusBar.superview.insertSubview(statusBarBlurView, belowSubview: statusBar)
+        self.view.addSubview(statusBarBlurView)
+
+        let statusBarBackgroundImage = UIImage(named: "MaskPixel").imageWithRenderingMode(.AlwaysTemplate)
+        let statusBarBackgroundView = UIImageView(image: statusBarBackgroundImage)
+        statusBarBackgroundView.frame = statusBarVibrancyView.bounds
+        statusBarBackgroundView.autoresizingMask = .FlexibleWidth | .FlexibleHeight
+        statusBarVibrancyView.contentView.addSubview(statusBarBackgroundView)
+    }
+
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+}
+
+extension ViewController : UIScrollViewDelegate {
+    func viewForZoomingInScrollView(scrollView: UIScrollView!) -> UIView! {
+        return scrollView.subviews.isEmpty ? nil : scrollView.subviews[0] as UIView
+    }
+
+    func scrollViewDidZoom(scrollView: UIScrollView!) {
+        setContentInsetToCenterScrollView(scrollView)
+    }
+
+    func setContentInsetToCenterScrollView(scrollView: UIScrollView) {
+        var leftInset: CGFloat = 0.0
+        if (scrollView.contentSize.width < scrollView.bounds.width) {
+            leftInset = (scrollView.bounds.width - scrollView.contentSize.width) / 2
+        }
+
+        var topInset: CGFloat = 0.0
+        if (scrollView.contentSize.height < scrollView.bounds.height) {
+            topInset = (scrollView.bounds.height - scrollView.contentSize.height) / 2
+        }
+        
+        scrollView.contentInset = UIEdgeInsets(top: topInset, left: leftInset, bottom: 0, right: 0)
+    }
+}
